5299b6d4ebd5d0e8bbaf55f7ba22d8eb53f7b6f7
Craig de Stigter <craig@destigter.nz>
2020-07-02 12:39:43 +1200

    0.6.0 release

--- fieldsignals/__init__.py
+++ fieldsignals/__init__.py
@@ -1,3 +1,3 @@
 from .signals import pre_save_changed, post_save_changed  # noqa
 
-__version__ = (0, 5, 0)
+__version__ = (0, 6, 0)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-fieldsignals",
-    version="0.5.0",
+    version="0.6.0",
     packages=["fieldsignals", "fieldsignals.tests"],
     include_package_data=True,
     test_suite="fieldsignals.tests.test_signals",
